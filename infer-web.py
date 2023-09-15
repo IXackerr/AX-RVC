92b2a58331f6b69d323af77778832c2e6267f3fc
Просто Хаскер <62063826+IXackerr@users.noreply.github.com>
2023-09-15 19:06:51 +0000

    ty9

--- infer-web.py
+++ infer-web.py
@@ -3082,13 +3082,17 @@ with gr.Blocks(theme=gr.themes.Soft(), title="Mangio-RVC-Web 💻") as app:
     if (
         config.iscolab or config.paperspace
     ):  # Share gradio link for colab and paperspace (FORK FEATURE)
-        app.queue(concurrency_count=511, max_size=1022).launch(share=false)
+        app.queue(concurrency_count=511, max_size=1022).launch(
+            server_port=config.listen_port,
+            share=False,
+            )
     else:
         app.queue(concurrency_count=511, max_size=1022).launch(
             server_name="0.0.0.0",
             inbrowser=not config.noautoopen,
             server_port=config.listen_port,
             quiet=False,
+            share=False,
         )
 
 # endregion
