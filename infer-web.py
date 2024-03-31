5887bc9dc72f9de12acbaffdea50c8dd24ceaa5a
Просто Хаскер <62063826+IXackerr@users.noreply.github.com>
2024-03-31 14:04:11 +0000

    remove test fix code

--- infer-web.py
+++ infer-web.py
@@ -902,19 +902,12 @@ with gr.Blocks(title="💙 AX-RVC WebUI 💎", theme=gr.themes.Base(primary_hue=
                                 label=i18n("变调(整数, 半音数量, 升八度12降八度-12)"),
                                 value=0,
                             )
-                            input_audio1 = gr.Dropdown(
+                            input_audio0 = gr.Dropdown(
                                 label=i18n(
                                     "输入待处理音频文件路径(默认是正确格式示例)"
                                 ),
                                 choices=sorted(audio_paths),
                                 value="",
-                                interactive=True,
-                            )
-                            input_audio0 = gr.Textbox(
-                                label=i18n(
-                                    "test audio path"
-                                ),
-                                placeholder="C:\\Users\\Desktop\\model_example.index",
                                 interactive=True,
                             )
                             file_index1 = gr.Textbox(
