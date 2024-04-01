aa20f0530b686c9bc3ba4f87bd781d38f7b03feb
Просто Хаскер <62063826+IXackerr@users.noreply.github.com>
2024-04-01 19:07:17 +0000

    test 3

--- infer-web.py
+++ infer-web.py
@@ -893,7 +893,7 @@ with gr.Blocks(title="💙 AX-RVC WebUI 💎", theme=gr.themes.Base(primary_hue=
                 with gr.Row():
                     with gr.Column():  # First column for audio-related inputs
                         dropbox = gr.File(label=i18n("Drag your audio here:"))
-                        filedrop = gr.File()
+                        gr.File(label=i18n("Test"))
                         record_button = gr.Audio(
                             sources="microphone",
                             label=i18n("Or record an audio:"),
