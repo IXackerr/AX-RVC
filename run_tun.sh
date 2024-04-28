dbe93b768482894fe6a61e0b68730a9e702e6627
Просто Хаскер <62063826+IXackerr@users.noreply.github.com>
2024-04-29 01:55:58 +0300

    Update run_tun.sh

--- run_tun.sh
+++ run_tun.sh
@@ -59,5 +59,7 @@ get_tunnel_info() {
 NGROK_PUBLIC_URL_1=$(get_tunnel_info "$TUNNEL_NAME_1")
 NGROK_PUBLIC_URL_2=$(get_tunnel_info "$TUNNEL_NAME_2")
 
+echo ""
 echo "AX RVC => [ $NGROK_PUBLIC_URL_1 ]"
 echo "Tensonboard => [ $NGROK_PUBLIC_URL_2 ]"
+echo ""
